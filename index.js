eb1538b8435cd9ac9e7b4043667196d490dc0489
Sheila Samita <samitasheila1@gmail.com>
2025-02-27 17:23:52 +0300

    objects

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+
+const employee = {
+    name: "Sam",
+    streetAddress: "123 Main St"
+};
+// Non-destructive update using the spread operator
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+}
+
+// Destructive update (modifies the original object)
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+// Non-destructive delete using the spread operator
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+// Destructive delete (modifies the original object)
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
